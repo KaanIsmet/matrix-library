0ec054728007ddbf189a887e8951d019cc3eb22f
kaanismet <kaanismet7@gmail.com>
2025-07-22 21:46:22 -0500

    added methods for matrix class

--- matrix.hpp
+++ matrix.hpp
@@ -1,20 +1,58 @@
 #ifndef MATRIX_LIBRARY_HPP
 #define MATRIX_LIBRARY_HPP
 
+#include <iostream>
 #include <concepts>
 #include <cstdint>
-#include <vector>
+
+template<typename T>
+concept Number = std::integral<T>;
+
 
 template <typename T>
-requires same_as<T, std::int64_t>
+requires Number<T>
 class Matrix {
 private:
 	const size_t ROWS;
 	const size_t COLS;
-	std::vector<T> data_;
+	T** data_;
 
 public:
-	Matrix(size_t rows, size_t cols) : ROWS(rows), COLS(cols), data_(rows * cols) {}
-	
+	Matrix(size_t rows, size_t cols) : ROWS(rows), COLS(cols) {
+		this.data_ = new int[ROWS * COLS];
+	}
 
+	Matrix(size_t rows, size_t cols, int64_t num) : ROWS(rows), COLS(cols) {
+		this.data_ = new int[ROWS * COLS];
+		fill(num);
+	}
+	
+	void fill(Matrix m, int64_t num) {
+		for (size_t i = 0; i < ROWS; i++) 
+			for (size_t j = 0; j < COLS; j++)
+				m->data_[i][j] = num;	
+	}
+
+	void zero(Matrix m) {
+		for (size_t i = 0; i < ROWS; i++) 
+			for (size_t j = 0; j < COLS; j++) 
+				m->data_[i][j] = 0;
+	}
+
+	
+		
+	bool equals(Matrix a, Matrix b) {
+		if (a.ROWS != b.ROWS || a.COLS != b.COLS)
+			return false;
+
+		for (int i = 0; i < a.ROWS; i++) 
+			for (int j = 0; j < a.COLS; j++)
+				if (a->data_[i][j] != b->data_[i][j])
+					return false;
+		return true;
+	}
+	
 };		
+
+
+#endif
